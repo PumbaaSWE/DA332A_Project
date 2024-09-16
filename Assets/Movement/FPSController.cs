14797a1c199ed140a27df0e6a50b10adb79fa8e4
Winn1337 <112430462+Winn1337@users.noreply.github.com>
2024-09-16 14:50:21 +0200

    Initial movement commit
    Just drag the Player prefab into a scene!
    
    [!] Temporary solution: Player collides with everything that isn't on "Ignore Raycast" layer, and Player is on that layer. Maybe add a "Player" layer, or solve in another way. [!]
    
    TODO:
    - Modify ControllerData or just use SerializeFields
    - Use new input system by updating/replacing KeyBinds
    - General clean up
    - Place assets in correct folders?

---
+++ Assets/Movement/FPSController.cs
@@ -0,0 +1,295 @@
+using UnityEngine;
+
+/// <summary>
+/// Partly stolen from BaseController by pumbaaswe, partly stolen from Sourcelike FPS Character Controller by Majikayo Games and partly original af.
+/// Author: Winn1337 / Johannes
+/// </summary>
+public class FPSController : MonoBehaviour
+{
+    public struct MoveCommand
+    {
+        public Vector2 move;
+        public Vector2 mouse;
+        public bool jump;
+        public bool crouch;
+        public bool crouchToggle;
+        public bool sprint;
+    }
+
+    [SerializeField] private KeyBinds keyBinds;
+    [SerializeField] private ControllerData controllerData;
+    [SerializeField] private float MaxAirAccel;
+    [SerializeField] private Transform head;
+    [SerializeField] private Vector3 velocity;
+    [SerializeField] private float speed;
+
+    private MoveCommand command;
+    private CapsuleCollider cc;
+    private Rigidbody rb;
+
+    private bool crouchToggle;
+    private bool grounded;
+    private bool wasGrounded;
+    private bool didJump;
+    private float xRot;
+    private Vector3 move;
+
+    private void Start()
+    {
+        cc = GetComponent<CapsuleCollider>();
+        cc.radius = controllerData.radius;
+        cc.height = controllerData.height;
+        cc.center = Vector3.up * controllerData.height / 2f;
+
+        rb = GetComponent<Rigidbody>();
+        move = Vector3.zero;
+
+        if (controllerData.jumpHeight > 0)
+        {
+            /* Math to compute impulse (force) to jump a set height
+             * impulse J = delta_p = m*v
+             * mv^2/2 = p^2/(2*m) = mgh => p^2 = 2 * m^2 * h etc...
+             */
+            // but we don't consider drag, maybe that's why it doesn't work on my controller?
+            float g = Physics.gravity.magnitude;
+            float m = rb.mass;
+            controllerData.jumpForce = Mathf.Sqrt(2 * m * m * g * controllerData.jumpHeight);
+        }
+    }
+
+    private void Update()
+    {
+        command = GetInput();
+
+        // Rotate body Y
+        transform.Rotate(Vector3.up, command.mouse.x);
+
+        // Rotate head X
+        if (head != null)
+        {
+            xRot = Mathf.Clamp(xRot - command.mouse.y, controllerData.minLookUpAngle, controllerData.maxLookUpAngle);
+            head.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+        }
+
+        // Jump
+        if (command.jump && grounded && !didJump)
+        {
+            rb.AddForce(Vector3.up * controllerData.jumpForce, ForceMode.Impulse);
+        }
+
+        // Crouch and uncrouch
+        if (command.crouch)
+        {
+            cc.height = Mathf.Max(cc.height - controllerData.crouchSpeed * Time.deltaTime, controllerData.crouchHeight);
+            cc.center = Vector3.up * cc.height / 2f;
+        }
+        else
+        {
+            // Check if we can uncrouch
+            bool hitHead = Physics.SphereCast(transform.position + Vector3.up * cc.radius, cc.radius, Vector3.up, out RaycastHit hit, cc.height + controllerData.crouchSpeed * Time.deltaTime - cc.radius * 2f, controllerData.groundLayer, QueryTriggerInteraction.Ignore);
+
+            if (!hitHead)
+            {
+                cc.height = Mathf.Min(cc.height + controllerData.crouchSpeed * Time.deltaTime, controllerData.height);
+                cc.center = Vector3.up * cc.height / 2f;
+            }
+        }
+        head.position = transform.position + Vector3.up * (cc.height - controllerData.camOffset);
+        bool crouched = cc.height != controllerData.height;
+
+        // Sum the move vector for fixed update
+        if (command.move.sqrMagnitude > 1f)
+            command.move.Normalize();
+
+        command.move.x *= controllerData.strafeMultiplier;
+        command.move.y *= command.move.y > 0f ? (crouched ? 1f : (command.sprint ? controllerData.sprintMultiplier : 1f)) : controllerData.backMultiplier;
+
+        if (crouched)
+            command.move *= controllerData.backMultiplier;
+
+        Vector3 wishDir = transform.right * command.move.x + transform.forward * command.move.y;
+
+        move += wishDir * Time.deltaTime;
+
+        // Debug
+        velocity = rb.velocity;
+        speed = rb.velocity.magnitude;
+
+        if (command.jump && !didJump)
+            didJump = true;
+    }
+
+    private void FixedUpdate()
+    {
+        float r = cc.radius;
+        float halfHeight = cc.height / 2;
+
+        wasGrounded = grounded;
+
+        if (rb.velocity.y > 0f)
+        {
+            grounded = false;
+        }
+        else
+        {
+            grounded = Physics.SphereCast(transform.position + Vector3.up * halfHeight, r, Vector3.down, out RaycastHit hit, halfHeight + .1f - r, controllerData.groundLayer, QueryTriggerInteraction.Ignore);
+        }
+
+        // Snap down if sent flying by ramps or stepping down small steps
+        if (wasGrounded && !didJump)
+        {
+            Physics.SphereCast(transform.position + Vector3.up * halfHeight, r, Vector3.down, out RaycastHit hit, halfHeight - r + controllerData.stepHeight, controllerData.groundLayer, QueryTriggerInteraction.Ignore);
+            
+            if (hit.distance > 0f && hit.point.y <= transform.position.y + .1f)
+            {
+                rb.MovePosition(transform.position.WithY(hit.point.y));
+
+                //If we move, we want to keep our Y velocity
+                if (move.sqrMagnitude > 0f)
+                {
+                    float normalVel = Vector3.Dot(rb.velocity, hit.normal);
+                    rb.velocity -= hit.normal * normalVel;
+                }
+                // Otherwise, we want to stay on the stairs. this is a bit hacky but it's to prevent the capsule collider to make us slide off the steps
+                else
+                {
+                    rb.velocity = rb.velocity.WithY(0f);
+                }
+
+                grounded = true;
+            }
+        }
+
+        //Snap up small steps
+        if (grounded && !didJump)
+        {
+            Physics.SphereCast(transform.position + rb.velocity.WithY(0) * Time.fixedDeltaTime + Vector3.up * halfHeight, r, Vector3.down, out RaycastHit hit, halfHeight - r, controllerData.groundLayer, QueryTriggerInteraction.Ignore);
+
+            if (hit.distance > 0f && hit.point.y <= transform.position.y + controllerData.stepHeight)
+            {
+                transform.position = transform.position.WithY(hit.point.y - Physics.gravity.y * Time.fixedDeltaTime * Time.fixedDeltaTime);
+
+                //If we move, we want to keep our Y velocity
+                if (move.sqrMagnitude > 0f)
+                {
+                    float normalVel = Vector3.Dot(rb.velocity, hit.normal);
+                    rb.velocity -= hit.normal * normalVel;
+                }
+                // Otherwise, we want to stay on the stairs. this is a bit hacky but it's to prevent the capsule collider to make us slide off the steps
+                //else
+                {
+                    rb.velocity = rb.velocity.WithY(0f);
+                }
+
+                grounded = true;
+            }
+        }
+
+        if (grounded)
+            DoGroundPhysics();
+        else
+            DoAirPhysics();
+
+        move = Vector3.zero;
+        didJump = false;
+
+        //Debug.Log(grounded);
+    }
+
+    private void DoGroundPhysics()
+    {
+        rb.drag = controllerData.groundDrag;
+
+        if (move.sqrMagnitude <= 0f)
+            return;
+
+        float speed = controllerData.moveSpeed;
+
+        // v = v0 + at
+        // a = (v - v0) / t
+        // F = m * a
+
+        // Gives player bunny hops by adding one ground frame with almost air movement
+        if (wasGrounded)
+        {
+            Vector3 v = move / Time.fixedDeltaTime * speed;
+            Vector3 a = (v - rb.velocity) / Time.fixedDeltaTime;
+            Vector3 f = rb.mass * a;
+            f = f.normalized * Mathf.Min(f.magnitude, controllerData.maxAccel);
+            rb.AddForce(f.WithY(0));
+        }
+        else
+        {
+            rb.drag = controllerData.airDrag;
+            float currentSpeedInWishDir = Vector3.Dot(rb.velocity, move / Time.fixedDeltaTime);
+
+            float v = speed;
+            float a = (v - currentSpeedInWishDir) / Time.fixedDeltaTime;
+            float f = rb.mass * a;
+            rb.AddForce((Mathf.Min(f, controllerData.maxAccel) * move / Time.fixedDeltaTime).WithY(0));
+        }
+    }
+    
+    private void DoAirPhysics()
+    {
+        rb.drag = controllerData.airDrag;
+
+        if (move.sqrMagnitude <= 0f)
+            return;
+
+        float currentSpeedInWishDir = Vector3.Dot(rb.velocity, move.normalized);
+
+        float v = controllerData.moveSpeed;
+        float a = (v - currentSpeedInWishDir) / Time.fixedDeltaTime;
+        float f = rb.mass * a;
+        rb.AddForce((Mathf.Min(f, MaxAirAccel) * move.normalized).WithY(0));
+
+    }
+
+    private MoveCommand GetInput()
+    {
+
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * keyBinds.sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * keyBinds.sensY;
+
+
+        float moveZ = 0;
+        float moveX = 0;
+        if (Input.GetKey(keyBinds.forward))
+        {
+            moveZ += 1;
+        }
+        if (Input.GetKey(keyBinds.back))
+        {
+            moveZ += -1;
+        }
+
+        if (Input.GetKey(keyBinds.right))
+        {
+            moveX += 1;
+        }
+        if (Input.GetKey(keyBinds.left))
+        {
+            moveX += -1;
+        }
+
+        if (Input.GetKeyDown(keyBinds.crouchToggle))
+        {
+            crouchToggle = true;
+        }
+        else if (Input.GetKeyUp(keyBinds.crouchToggle))
+        {
+            crouchToggle = false;
+        }
+
+        return new MoveCommand()
+        {
+            mouse = new Vector2(mouseX, mouseY) * keyBinds.sens,
+            move = new Vector2(moveX, moveZ),
+            jump = Input.GetKey(keyBinds.jump),
+            crouch = Input.GetKey(keyBinds.crouch),
+            crouchToggle = crouchToggle,
+            sprint = Input.GetKey(keyBinds.sprint),
+        };
+    }
+}
