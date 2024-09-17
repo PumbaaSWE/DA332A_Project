19d75a84a5f19ab6f0e64f1ab15e13b1d0170cc2
OperatorAlexCode <alexander.jr.karlsson@gmail.com>
2024-09-17 11:06:35 +0200

    The player can now fire and reload a gun

--- Assets/_Scripts/Firearm.cs
+++ Assets/_Scripts/Firearm.cs
@@ -1,14 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using static UnityEngine.InputSystem.InputAction;
 
 public class Firearm : MonoBehaviour
 {
-    public int CurrentMagazine, ReserveAmmo, MaxReserveAmmo;
+    public Transform ShotOrigin;
+    public int LoadedAmmo, MagazineSize, ReserveAmmo, MaxReserveAmmo;
     public int FireRate;
 
-    //public float VerticalRecoil;
-    //public float MaxHorizontalRecoil;
+    public float VerticalRecoil, MaxHorizontalRecoilLeft, MaxHorizontalRecoilRight;
+    public float MaxRange = 100;
+
+    [SerializeField] bool RoundInTheChamber = true;
+    [SerializeField] bool AutoReload = false;
+    bool CanFire = true;
+    bool Firing = false;
+    [SerializeField] LayerMask ShootableLayers;
+    [SerializeField] GameObject Decal;
 
     //[SerializeField] FireMode CurrentMode;
     //[SerializeField] []FireMode AvailableModes;
@@ -16,7 +27,7 @@ public class Firearm : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        LoadedAmmo = MagazineSize + Convert.ToInt32(RoundInTheChamber);
     }
 
     // Update is called once per frame
@@ -25,22 +36,56 @@ public class Firearm : MonoBehaviour
 
     }
 
-    public void Fire()
+    public void Fire(CallbackContext context)
     {
-        if (CurrentMagazine > 0)
+        if (context.started)
         {
-            
+            if (LoadedAmmo > 0)
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(ShotOrigin.position, ShotOrigin.forward, out hit, MaxRange))
+                {
+                    Debug.DrawLine(ShotOrigin.position, hit.point, Color.red);
+                    Debug.Log($"Hit object {hit.collider.gameObject.name} at {hit.point}");
+                    Instantiate(Decal, hit.point, new Quaternion());
+                }
+
+                else
+                    Debug.DrawRay(ShotOrigin.position, ShotOrigin.forward, Color.red);
+
+                LoadedAmmo--;
+            }
+
+            Debug.Log($"Mag:{LoadedAmmo} | Reserve: {ReserveAmmo}");
         }
     }
 
-    public void AimDownSights()
+    public void AimDownSights(CallbackContext context)
     {
 
     }
 
-    public void Reload()
+    public void Reload(CallbackContext context)
     {
+        if (context.phase == UnityEngine.InputSystem.InputActionPhase.Performed)
+        {
+            if (ReserveAmmo == 0)
+                return;
 
+            int returnedAmmo = Mathf.Clamp(LoadedAmmo - Convert.ToInt32(RoundInTheChamber), 0, LoadedAmmo);
+
+            LoadedAmmo -= returnedAmmo;
+
+            ReserveAmmo += returnedAmmo;
+
+            int ammoToLoad = Mathf.Clamp(ReserveAmmo, 0, MagazineSize);
+
+            LoadedAmmo += ammoToLoad;
+
+            ReserveAmmo -= ammoToLoad;
+
+            Debug.Log($"Mag:{LoadedAmmo} | Reserve: {ReserveAmmo}");
+        }
     }
 }
 
