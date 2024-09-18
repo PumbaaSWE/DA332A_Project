8e0b320b2bca72eb8cedacfa419ee97ee690318f
OperatorAlexCode <alexander.jr.karlsson@gmail.com>
2024-09-18 11:21:03 +0200

    ADS & hipfire implemented
    You can now aim down sight of guns and it now has hipfire when not in ADS

--- Assets/_Scripts/Firearm.cs
+++ Assets/_Scripts/Firearm.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 using static UnityEngine.InputSystem.InputAction;
+using static UnityEngine.InputSystem.LowLevel.InputStateHistory;
 using Random = UnityEngine.Random;
 
 public class Firearm : MonoBehaviour
@@ -14,14 +15,18 @@ public class Firearm : MonoBehaviour
     public int RPM;
     int CurrentBurst;
 
-    public float VerticalRecoil, MinHorizontalRecoil, MaxHorizontalRecoil;
-    public float MaxRange = 100;
+    [SerializeField] float VerticalRecoil, MinHorizontalRecoil, MaxHorizontalRecoil;
+    public float AdsZoom, OriginalFov;
+    [SerializeField] float MaxRange = 100;
+    [SerializeField] float HipFireAngle, MinHipFireAngle, MaxHipFireAngle, HipFireGain, HipFireDecay;
+    [SerializeField] float RecoilDuration;
+    Vector2 RecoilImpulse;
 
     [SerializeField] bool RoundInTheChamber = true;
     [SerializeField] bool AutoReload = false;
     bool CanFire = true;
     bool Firing = false;
-    bool ADS = false;
+    [SerializeField] bool ADS = false;
     [SerializeField] LayerMask ShootableLayers;
     [SerializeField] GameObject Decal;
     [SerializeField] FPSController Player;
@@ -33,12 +38,17 @@ public class Firearm : MonoBehaviour
     void Start()
     {
         LoadedAmmo = MagazineSize + Convert.ToInt32(RoundInTheChamber);
+        HipFireAngle = MinHipFireAngle;
+        OriginalFov = GameObject.Find("Main Camera").GetComponent<Camera>().fieldOfView;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (HipFireAngle > MinHipFireAngle && !Firing)
+        {
+            HipFireAngle = Mathf.Clamp(MinHipFireAngle, MaxHipFireAngle, HipFireAngle);
+        }
     }
 
     public void Fire(CallbackContext context)
@@ -64,21 +74,32 @@ public class Firearm : MonoBehaviour
         if (CanFire && Firing)
         {
             RaycastHit hit;
-            if (Physics.Raycast(ShotOrigin.position, ShotOrigin.forward, out hit, MaxRange))
+            Vector3 shotDirection = ShotOrigin.forward;
+
+            if (!ADS)
             {
-                Debug.DrawLine(ShotOrigin.position, hit.point, Color.red);
+                Vector2 randomPoint = Random.insideUnitCircle;
+                shotDirection = Quaternion.Euler(randomPoint.x * HipFireAngle, randomPoint.y * HipFireAngle, 0) * shotDirection;
+                HipFireAngle = Mathf.Clamp(MinHipFireAngle, MaxHipFireAngle, HipFireAngle + HipFireGain);
+            }
+
+            if (Physics.Raycast(ShotOrigin.position, shotDirection, out hit, MaxRange))
+            {
+                Debug.DrawLine(ShotOrigin.position, hit.point, Color.red, 10f);
                 Debug.Log($"Hit object {hit.collider.gameObject.name} at {hit.point}");
                 Instantiate(Decal, hit.point, new Quaternion());
-                Player.Rotate(VerticalRecoil,Random.Range(MinHorizontalRecoil,MaxHorizontalRecoil));
             }
 
             else
-                Debug.DrawRay(ShotOrigin.position, ShotOrigin.forward, Color.red);
+                Debug.DrawRay(ShotOrigin.position, shotDirection, Color.red, 10f);
 
             LoadedAmmo--;
             Debug.Log($"Mag:{LoadedAmmo} | Reserve: {ReserveAmmo}");
 
             CanFire = false;
+            Player.Rotate(VerticalRecoil, Random.Range(MinHorizontalRecoil, MaxHorizontalRecoil));
+            //StopCoroutine(Recoil());
+            //StartCoroutine(Recoil());
             yield return new WaitForSeconds(60f / (float)(RPM));
             CanFire = true;
 
@@ -100,11 +121,43 @@ public class Firearm : MonoBehaviour
         }
     }
 
+    IEnumerator Recoil()
+    {
+        float recoilDuration = RecoilDuration;
+        float timeElapsed = 0f;
+
+        float xRecoil = VerticalRecoil;
+        float yRecoil = Random.Range(MinHorizontalRecoil, MaxHorizontalRecoil);
+        float previousXRecoil = 0;
+        float previousYRecoil = 0;
+
+        while (timeElapsed < recoilDuration)
+        {
+            Vector2 recoil = new(Mathf.Lerp(0, xRecoil, timeElapsed / recoilDuration), Mathf.Lerp(0, yRecoil, timeElapsed / recoilDuration));
+
+            Player.Rotate(recoil.x - previousXRecoil, recoil.y - previousYRecoil);
+
+            previousXRecoil = recoil.x;
+            previousYRecoil = recoil.y;
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        Player.Rotate(xRecoil - previousXRecoil, yRecoil - previousYRecoil);
+    }
+
     public void AimDownSights(CallbackContext context)
     {
         if (context.phase == UnityEngine.InputSystem.InputActionPhase.Performed)
         {
-            
+            ADS = true;
+            GameObject.Find("Main Camera").GetComponent<Camera>().fieldOfView = OriginalFov / AdsZoom;
+        }
+
+        else if (context.canceled)
+        {
+            ADS = false;
+            GameObject.Find("Main Camera").GetComponent<Camera>().fieldOfView = OriginalFov;
         }
     }
 
