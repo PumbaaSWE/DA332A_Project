4746b1d8f9428165ae4f8b19762979123177e6d5
PumbaaSWE <ballejalle@hotmail.com>
2024-09-23 10:54:55 +0200

    Test scene updated
    Added temporary HUD for firearm

--- Assets/_Scripts/HUD/FirearmHUD.cs
+++ Assets/_Scripts/HUD/FirearmHUD.cs
@@ -1,18 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class FirearmHUD : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    Firearm firearm;
+    [SerializeField] TMP_Text text;
+
+
+    public void SetFirearm(Firearm firearm)
+    {
+        this.firearm = firearm;
+    }
+
+
     void Start()
     {
-        
+        if (!firearm)
+        {
+            firearm = FindAnyObjectByType<Firearm>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (firearm)
+        {
+            text.text = firearm.LoadedAmmo.ToString();
+        }
     }
 }
