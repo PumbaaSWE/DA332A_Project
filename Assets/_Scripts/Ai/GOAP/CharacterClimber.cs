a19a7483b3037ebd0f48bb85517e470b6275233c
Arvid98 <111972825+Arvid98@users.noreply.github.com>
2024-09-24 17:53:45 +0100

    Navesh on wall.
    The ai does not have an navmesh agent. But is uses the navmesh suface to get movetargets

--- Assets/_Scripts/Ai/GOAP/CharacterClimber.cs
+++ Assets/_Scripts/Ai/GOAP/CharacterClimber.cs
@@ -31,33 +31,224 @@ public class CharacterClimber : CharacterBase
     EnemyAI linkedAI;
 
     internal Animator animator;
+    [SerializeField] float NearestPointSearchRange = 7f;
+    private NavMeshPath navMeshPath;
+    private float pathDistance;
+
+    public WallClimber wallClimber;
+    public NavMeshAgent navAgent;
 
     private void Awake()
     {
         controller = GetComponent<MoveTowardsController>();
+        wallClimber = GetComponent<WallClimber>();
+        navMeshPath = new NavMeshPath();
+        navAgent = GetComponent<NavMeshAgent>();
     }
 
     protected override void Start()
     {
         base.Start();
+      
         linkedAI = GetComponent<EnemyAI>();
     }
+    private float CalculateNavMeshPath(Vector3 destination)
+    {
+        if (NavMesh.CalculatePath(transform.position, destination, NavMesh.AllAreas, navMeshPath))
+        {
+            pathDistance = GetPathLength(navMeshPath);
+            return pathDistance;
+        }
+        return Mathf.Infinity;
+    }
+
+    private float GetPathLength(NavMeshPath path)
+    {
+        float totalLength = 0.0f;
+        for (int i = 0; i < path.corners.Length - 1; i++)
+        {
+            totalLength += Vector3.Distance(path.corners[i], path.corners[i + 1]);
+        }
+        return totalLength;
+    }
+
+    private float CalculateClimbOverDistance(Vector3 wallPosition, Vector3 destination)
+    {
+        float distanceToWall = Vector3.Distance(transform.position, wallPosition);
+
+        RaycastHit hitTop;
+        if (Physics.Raycast(wallPosition, Vector3.forward, out hitTop, Mathf.Infinity, LayerMask.GetMask("Wall")))
+        {
+
+            GameObject wallObject = hitTop.collider.gameObject;
+
+
+            float wallHeight = wallObject.GetComponent<Collider>().bounds.size.y;
+            Debug.Log("Wall height: " + wallHeight);
+
+            float distanceAfterClimb = Vector3.Distance(wallPosition + Vector3.up * wallHeight, destination);
+
+            return distanceToWall + wallHeight + distanceAfterClimb;
+        }
+        else
+        {
+           
+            return Mathf.Infinity;  
+        }
+    }
+
 
     protected override void Update()
     {
         base.Update();
 
-       // CheckGroundUpright();
+        // CheckGroundUpright();
         if (DestinationSet && IsAtDestination())
         {
             DestinationSet = false;
             ReachedDestination = true;
-         
+
             Debug.Log("Destination reached.");
         }
 
+        //if (!navAgent.pathPending && !navAgent.isOnOffMeshLink && DestinationSet && (navAgent.remainingDistance <= navAgent.stoppingDistance))
+        //{
+        //    DestinationSet = false;
+        //    ReachedDestination = true;
+        //    // make agent idle
+        //}
+
+
+
         UpdateState();
     }
+    void WallMovment(Vector3 destination)
+    {
+        Vector3 wallPosition = Vector3.zero;
+
+        int steps = 10;
+        Vector3 direction = (destination - transform.position).normalized;
+        float distanceToDestination = Vector3.Distance(transform.position, destination);
+        float stepSize = distanceToDestination / steps;
+
+        //for (int i = 1; i <= steps; i++)
+        //{
+        //    Vector3 stepPosition = transform.position + direction * stepSize * i;
+        //    if (Physics.Raycast(stepPosition, direction, out RaycastHit hit, stepSize, LayerMask.GetMask("Wall")))
+        //    {
+        //        wallPosition = hit.point;
+        //        break; 
+        //    }
+        //}
+        //if (wallPosition == Vector3.zero)
+        //{
+         
+        //    SetDestinationNav(destination);
+        //    return;
+        //}
+
+        //float navMeshPathDistance = CalculateNavMeshPath(destination);
+        //float climbOverDistance = CalculateClimbOverDistance(wallPosition, destination);
+
+        //if (climbOverDistance < navMeshPathDistance)
+        //{
+        //    StartClimbing(wallPosition);
+        //}
+        //else
+        {
+            
+            SetDestinationNav(destination);
+        }
+    }
+   
+
+    public virtual void SetDestinationNav(Vector3 destination)
+    {
+        NavMeshHit hitResult;
+        if (NavMesh.SamplePosition(destination, out hitResult, NearestPointSearchRange, NavMesh.AllAreas))
+        {
+            NavMeshPath path = new NavMeshPath();
+            if (NavMesh.CalculatePath(transform.position, hitResult.position, NavMesh.AllAreas, path))
+            {
+                StartCoroutine(FollowNavMeshPath(path));
+
+                DestinationSet = true;
+                ReachedDestination = false;
+                Debug.Log("Destination set to: " + hitResult.position);
+            }
+        }
+    }
+
+    private IEnumerator FollowNavMeshPath(NavMeshPath path)
+    {
+        for (int i = 1; i < path.corners.Length; i++)
+        {
+            Vector3 corner = path.corners[i];
+            Debug.Log("Moving to corner " + i);
+
+            NavMeshHit hit;
+            if (NavMesh.SamplePosition(corner, out hit, 1f, NavMesh.AllAreas))
+            {
+                int areaMask = hit.mask;
+
+                if (NavMesh.GetAreaFromName("Wall") == areaMask)
+                {
+                  
+                }
+                else if (NavMesh.GetAreaFromName("Walkable") == areaMask)
+                {
+                    
+                }
+            }
+
+     
+            controller.SetTarget(corner);
+
+            while (Vector3.Distance(transform.position, corner) > controller.stoppingDistance + 0.1f)
+            {
+                yield return new WaitForSeconds(0.1f);  
+            }
+        }
+    }
+
+    public Vector3 GetClosestNavMeshPoint(Vector3 currentPosition)
+    {
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(currentPosition, out hit, 10, NavMesh.AllAreas))
+        {
+
+            return hit.position;
+        }
+        else
+        {
+
+            return currentPosition;  
+        }
+    }
+
+
+
+    private void StartClimbing(Vector3 wallPosition)
+    {
+        StartCoroutine(ClimbOverWall(wallPosition));
+    }
+
+    private IEnumerator ClimbOverWall(Vector3 wallPosition)
+    {
+        while (Vector3.Distance(transform.position, wallPosition) > 0.1f)
+        {
+          
+            Vector3 climbDirection = Vector3.up;
+            controller.SetTarget(transform.position + climbDirection * Time.deltaTime);
+
+            yield return null;  
+        }
+
+  
+    }
+
+ 
+
     private bool IsAtDestination()
     {
         float sqrDistanceToDestination = (transform.position - controller.point).sqrMagnitude;
@@ -76,10 +267,10 @@ public class CharacterClimber : CharacterBase
         switch (agentState)
         {
             case AgentState.Normal:
-                // Hantera normal rörelse
+      
                 break;
             case AgentState.Armless:
-                // Hantera armless tillstånd
+           
                 break;
             case AgentState.Blind:
                 if (isBlind)
@@ -110,10 +301,10 @@ public class CharacterClimber : CharacterBase
 
     public virtual void SetDestination(Vector3 destination)
     {
-        
-        controller.SetTarget(destination);
-        DestinationSet = true;
-        ReachedDestination = false;
+        WallMovment(destination);
+        //controller.SetTarget(destination);
+        //DestinationSet = true;
+        //ReachedDestination = false;
         Debug.Log("Destination set to: " + destination);
         Debug.Log("Pos is: " + this.transform.position);
     }
@@ -124,39 +315,50 @@ public class CharacterClimber : CharacterBase
         ReachedDestination = false;
         DestinationSet = false;
     }
-
     public Vector3 PickLocationInRange(float range)
     {
         Vector3 searchLocation = transform.position;
-
-        searchLocation += Random.Range(-range, range) * transform.forward;
-        searchLocation += Random.Range(-range, range) * transform.right;
-
-        float raycastDistance = 10f; 
-        LayerMask groundMask = LayerMask.GetMask("Ground"); 
-
-        RaycastHit hitResult;
-
-        List<Vector3> directions = new List<Vector3>()
-    {
-        Vector3.down,       
-        transform.forward,  
-        -transform.forward, 
-        transform.right,    
-        -transform.right    
-    };
-
-
-        foreach (Vector3 direction in directions)
+        searchLocation += Random.Range(-range, range) * Vector3.forward;
+        searchLocation += Random.Range(-range, range) * Vector3.right;
+        NavMeshHit hitResult;
+        if (NavMesh.SamplePosition(searchLocation, out hitResult, NearestPointSearchRange, NavMesh.AllAreas))
         {
-            if (Physics.Raycast(searchLocation, direction, out hitResult, raycastDistance, groundMask))
-            {
-                return hitResult.point;
-            }
+            return hitResult.position;
         }
-
-        return searchLocation;
+        return transform.position;
     }
+    //public Vector3 PickLocationInRange(float range)
+    //{
+    //    Vector3 searchLocation = transform.position;
+
+    //    searchLocation += Random.Range(-range, range) * transform.forward;
+    //    searchLocation += Random.Range(-range, range) * transform.right;
+
+    //    float raycastDistance = 10f; 
+    //    LayerMask groundMask = LayerMask.GetMask("Default"); 
+
+    //    RaycastHit hitResult;
+
+    //    List<Vector3> directions = new List<Vector3>()
+    //{
+    //    Vector3.down,       
+    //    transform.forward,  
+    //    -transform.forward, 
+    //    transform.right,    
+    //    -transform.right    
+    //};
+
+
+    //    foreach (Vector3 direction in directions)
+    //    {
+    //        if (Physics.Raycast(searchLocation, direction, out hitResult, raycastDistance, groundMask))
+    //        {
+    //            return hitResult.point;
+    //        }
+    //    }
+
+    //    return searchLocation;
+    //}
 
     public void Attack()
     {
