bcae03d05d357b45806b40792bbe5c38927235b6
Monika_Sliauteryte <monika.sliauteryte@gmail.com>
2024-09-27 07:13:25 +0200

    Destroyable in scene Monika

--- Assets/Level_Design/Scripts/BoxItem.cs
+++ Assets/Level_Design/Scripts/BoxItem.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class BoxItem : MonoBehaviour
 {
-    
+    public GameObject cube;
     public void ChangeColor()
     {
         gameObject.GetComponent<Renderer>().material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+    }
+
+    public void SetActiveCubes()
+    {
+        cube.SetActive(true);
+    }
+
+    public void SetUnActiveCubes()
+    {
+        cube.SetActive(false);
     }
 
 }
