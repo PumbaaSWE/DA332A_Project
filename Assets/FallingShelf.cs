028d733b027d79d5d54ec0f954e189b88a6cc39b
Winn1337 <112430462+Winn1337@users.noreply.github.com>
2024-10-02 20:00:28 +0200

    Falling Shelf
    Added falling shelf to Prototype 1 scene
    
    Warning for very prototypy scripts. We will make them more generic later so that we don't need to write a separate script for every event that happens in the game! Perhaps using animations? Or events. Or both? :100:

---
+++ Assets/FallingShelf.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingShelf : MonoBehaviour
+{
+    [SerializeField] Vector3 localEndPos;
+    [SerializeField] Vector3 localEndRot;
+    [SerializeField] float length;
+    [SerializeField] AnimationCurve posCurve;
+    [SerializeField] AnimationCurve rotCurve;
+
+    float t;
+    Vector3 localStartPos;
+    Vector3 localStartRot;
+
+    private void OnEnable()
+    {
+        localStartPos = transform.localPosition;
+        localStartRot = transform.localRotation.eulerAngles;
+    }
+
+    void Update()
+    {
+        t += Time.deltaTime;
+
+        if (t >= length)
+        {
+            t = length;
+            enabled = false;
+        }
+
+        float tPos = posCurve.Evaluate(t / length);
+        transform.localPosition = Vector3.Lerp(localStartPos, localEndPos, tPos);
+
+        float tRot = rotCurve.Evaluate(t / length);
+        transform.localRotation = Quaternion.Lerp(Quaternion.Euler(localStartRot), Quaternion.Euler(localEndRot), tRot);
+    }
+
+    [MakeButton]
+    void SetEndPos()
+    {
+        localEndPos = transform.localPosition;
+    }
+
+    [MakeButton]
+    void SetEndRot()
+    {
+        localEndRot = transform.localRotation.eulerAngles;
+    }
+}
