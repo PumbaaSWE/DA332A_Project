285908c19ba25ed3c01b2ac099ee36732cb50c17
PumbaaSWE <ballejalle@hotmail.com>
2024-10-18 00:09:06 +0200

    Added TwoBoneIK
    Might be usefull later

---
+++ Assets/_Scripts/IK_Stuff/TwoBoneIK.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+
+public class TwoBoneIK : MonoBehaviour
+{
+
+    [Header("Bones")]
+    [Tooltip("Assign tip (i.e. hand), then right click on component >Setup from tip")]
+    [SerializeField] private Transform tip;
+    [SerializeField] private Transform mid;
+    [SerializeField] private Transform root;
+    [Header("Settings")]
+    [SerializeField] private Transform target;
+    [SerializeField] private Transform pole;
+
+    [SerializeField] private float rootElbowRotation;//Rotation offsets
+    [SerializeField] private float midElbowRotation;
+
+    float rootLength, midLength;
+
+
+    void Awake()
+    {
+        rootLength = (root.position - mid.position).magnitude;
+        midLength = (mid.position - tip.position).magnitude;
+    }
+
+
+
+
+    private void Solve()
+    {
+
+        float a = rootLength;
+        float b = midLength;
+        float c = Vector3.Distance(root.position, target.position);
+        Vector3 en = Vector3.Cross(target.position - root.position, pole.position - root.position);
+
+        Debug.DrawLine(root.position, target.position);
+        Debug.DrawLine((root.position + target.position) / 2, mid.position);
+
+        //Set the rotation of the root arm
+        root.rotation = Quaternion.LookRotation(target.position - root.position, Quaternion.AngleAxis(rootElbowRotation, mid.position - root.position) * en);
+        root.rotation *= Quaternion.Inverse(Quaternion.FromToRotation(Vector3.forward, mid.localPosition));
+        root.rotation = Quaternion.AngleAxis(-CosAngle(a, c, b), -en) * root.rotation;
+
+        //set the rotation of the mid arm
+        mid.rotation = Quaternion.LookRotation(target.position - mid.position, Quaternion.AngleAxis(midElbowRotation, tip.position - mid.position) * en);
+        mid.rotation *= Quaternion.Inverse(Quaternion.FromToRotation(Vector3.forward, tip.localPosition));
+
+        //mid.LookAt(mid, Pole.position - root.position);
+        //mid.rotation = Quaternion.AngleAxis(CosAngle(a, b, c), en);
+        tip.rotation = target.rotation;
+
+        //function that finds angles using the cosine rule 
+        static float CosAngle(float a, float b, float c)
+        {
+            if (!float.IsNaN(Mathf.Acos((-(c * c) + (a * a) + (b * b)) / (-2 * a * b)) * Mathf.Rad2Deg))
+            {
+                return Mathf.Acos((-(c * c) + (a * a) + (b * b)) / (2 * a * b)) * Mathf.Rad2Deg;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+
+    void Update()
+    {
+        Solve();
+    }
+
+    [ContextMenu("Setup from tip")]
+    public void SetUpFromTip()
+    {
+        mid = tip.parent;
+        root = mid.parent;
+        if (target == null)
+        {
+            GameObject t = new GameObject(gameObject.name + "_target");
+            t.transform.parent = transform;
+            t.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
+            target = t.transform;
+        }
+        if (pole == null)
+        {
+            GameObject t = new GameObject(gameObject.name + "_pole");
+            t.transform.parent = transform;
+            t.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
+            pole = t.transform;
+        }
+    }
+}
