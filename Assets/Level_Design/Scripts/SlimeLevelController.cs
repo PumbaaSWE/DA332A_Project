8643c2c81c068f1e4955e162b12b88c39d6cece8
Monika_Sliauteryte <monika.sliauteryte@gmail.com>
2024-10-18 13:26:44 +0200

    Level3_Objective_added
    Collect slime from enemies blood

---
+++ Assets/Level_Design/Scripts/SlimeLevelController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeLevelController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Material material;
+    float fulness;
+    public SlimePlane splane;
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+        fulness = material.GetFloat("_Fulness");
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void IncreaseSlime()
+    {
+        material.color = Color.red;
+        
+        fulness += splane.slimeCounter;
+        material.SetFloat("_Fulness", fulness);
+    }
+}
