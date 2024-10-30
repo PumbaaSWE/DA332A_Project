f9123745f65ccb84f1e6c2d88db50609f5d2309d
Winn1337 <112430462+Winn1337@users.noreply.github.com>
2024-10-30 15:49:26 +0100

    Update GameData.cs
    hardcoded some default values but we could get them from scriptable object maybe?

--- Assets/_Scripts/Persistence/GameData.cs
+++ Assets/_Scripts/Persistence/GameData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JetBrains.Annotations;
+using System;
 
 [Serializable]
 public class GameData
@@ -10,6 +11,30 @@ public class GameData
         // TODO: determine default data. What should player get at start of the first level?
         // start weapon?
         // most important, health? start with max i guess, but where do we get it from?
+        // i will hardcode some stuff for now, because spawning empty player is annoying
+
+        playerData.health = 100;
+
+        playerData.numFlares = 5;
+        playerData.numPistolRounds = 90;
+        playerData.numRifleRounds = 120;
+        playerData.numShotgunShells = 40;
+
+        playerData.weaponData = new WeaponData[]
+        {
+            new WeaponData()
+            {
+                id = 0,
+                ammo = 15,
+            },
+            new WeaponData()
+            {
+                id = 1,
+                ammo = 20
+            }
+        };
+
+        playerData.equippedWeapon = 1;
     }
 
     public string name;
