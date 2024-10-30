4b3f19eb63f1f9fb205b18dd9918d414f3025f3a
Winn1337 <112430462+Winn1337@users.noreply.github.com>
2024-10-30 19:04:56 +0100

    Elevator music less hearable through walls
    Also added non occlusion zone that you can place near the elevator where you don't want the elevator to be quiet

--- Assets/_Scripts/Audio/OccludedAudioSource.cs
+++ Assets/_Scripts/Audio/OccludedAudioSource.cs
@@ -6,15 +6,31 @@ public class OccludedAudioSource : MonoBehaviour
     [SerializeField] float occludedCutOffFreq = 1000f;
     [SerializeField] float defaultCutOffFreq = 22000f;
     [SerializeField] float lerpSpeed = 2f;
+    [SerializeField] float occludedVolumeScale = 0.5f;
+    [SerializeField] Transform nonOcclusionZone;
 
     bool occluded;
     float t;
+    float defaultVolume;
     AudioLowPassFilter filter;
+    AudioSource source;
     Transform player;
+    Collider[] colliders;
 
     void Start()
     {
         filter = GetComponent<AudioLowPassFilter>();
+        source = GetComponent<AudioSource>();
+
+        defaultVolume = source.volume;
+
+        if (nonOcclusionZone == null)
+            return;
+
+        colliders = nonOcclusionZone.GetComponentsInChildren<Collider>();
+
+        foreach(var collider in colliders)
+            collider.isTrigger = true;
     }
 
     void FixedUpdate()
@@ -28,14 +44,35 @@ public class OccludedAudioSource : MonoBehaviour
         }
 
         Vector3 pos = transform.position;
-        Vector3 dir = player.transform.position - pos;
+        Vector3 dir = player.position - pos;
         float dist = dir.magnitude;
 
         bool hit = Physics.Raycast(pos, dir, out RaycastHit hitInfo, dir.magnitude, Physics.AllLayers, QueryTriggerInteraction.Ignore);
 
+
         occluded = hit && hitInfo.transform != player;
 
+        if (occluded && colliders != null)
+        {
+            foreach (var collider in colliders)
+            {
+                Debug.Log("checking collider");
+                if (collider.bounds.Contains(player.position))
+                {
+                    Debug.Log("not occluded!");
+                    occluded = false;
+                    break;
+                }
+            }
+        }
+
         t = Mathf.Clamp01(t + Time.fixedDeltaTime * (occluded ? lerpSpeed : -lerpSpeed));
+
+        // keeps updating volume in case it changes in runtime, might be unnecessary
+        if (t == 0)
+            defaultVolume = source.volume;
+
         filter.cutoffFrequency = Mathf.Lerp(defaultCutOffFreq, occludedCutOffFreq, t);
+        source.volume = defaultVolume * Mathf.Lerp(1, occludedVolumeScale, t);
     }
 }
