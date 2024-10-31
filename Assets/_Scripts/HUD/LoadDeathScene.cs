6481be4999a8c62fb77ec8d4ec3e813abc19599c
Winn1337 <112430462+Winn1337@users.noreply.github.com>
2024-10-31 11:41:40 +0100

    Update LoadDeathScene.cs
    more simple

--- Assets/_Scripts/HUD/LoadDeathScene.cs
+++ Assets/_Scripts/HUD/LoadDeathScene.cs
@@ -1,9 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-#if UNITY_EDITOR
-using UnityEditor.SceneManagement;
-#endif
 public class LoadDeathScene : MonoBehaviour
 {
     string death = "Death";
@@ -19,25 +16,11 @@ public class LoadDeathScene : MonoBehaviour
                 SceneManager.LoadSceneAsync(deathScene.SceneName, LoadSceneMode.Additive);
             }
         }
-        else
-        {
-#if UNITY_EDITOR
-            Scene scene = EditorSceneManager.GetSceneByName(death);
-            if (!scene.IsValid())
-            {
-                EditorSceneManager.OpenScene("Assets/Scenes/HUD/HUD.unity", OpenSceneMode.Additive);
-            }
-#endif  
-        }
     }
 
     public void UnloadScene()
     {
         if (Application.isPlaying)
             SceneManager.UnloadSceneAsync(death);
-#if UNITY_EDITOR
-        else
-            EditorSceneManager.CloseScene(SceneManager.GetSceneByName(deathScene.SceneName), true);
-#endif
     }
 }
