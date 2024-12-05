e26335ecdaa62afbd32d3f8891ec678c6eaa99d6
Monika_Sliauteryte <monika.sliauteryte@gmail.com>
2024-12-05 12:47:06 +0100

    hobb-comm-it

--- Assets/HeatMap/Scripts/HeatMap.cs
+++ Assets/HeatMap/Scripts/HeatMap.cs
@@ -25,10 +25,19 @@ public class HeatMap : MonoBehaviour
    
     public GameObject plane;
 
+    float[,] colorValues; //20 20
+
+    int[] convertedXes;
+
+    int[] ints;
+
 
     public void InitializeComponents()
     {
-        
+        colorValues = new float[200,200];
+        convertedXes = new int[200];
+        ints = new int[200];
+        colorValues = InitializeColorValues(ints);
         lineRenderer = GetComponent<LineRenderer>();
         listObjects = new List<GameObject>();
         dataPosition = new Vector3[data.Length];
@@ -40,6 +49,9 @@ public class HeatMap : MonoBehaviour
         filePath = Path.GetDirectoryName(Application.dataPath) + "/Assets/HeatMap/SessionData/" + fileParentName + "/ " + fileName + ".json";
         CreateTexture();
         Debug.Log("Initializing");
+       
+
+
         
     }
 
@@ -126,6 +138,14 @@ public class HeatMap : MonoBehaviour
             dataPosition[i] = data[i].playerPos;
         }
         Debug.Log("creating vecxtor3 array Positions");
+        convertedXes = ConvertX(dataPosition);
+    
+        ints = HowManyInts(convertedXes);
+        Debug.Log(ints.Length);
+        for (int i = 0;i < ints.Length;i++)
+        {
+            Debug.Log(ints[i]);
+        }
     }
 
     public void DrawLines()
@@ -157,6 +177,63 @@ public class HeatMap : MonoBehaviour
 
     }
 
+    public void CreateTexture()
+    {
+        var texture = new Texture2D(200, 200);
+        float a;
+        //Color color = new Color(a, a, a);
+        //Debug.Log(color);
+        for (int i = 0; i < 200; i++)
+        {
+            for (int j = 0; j < 200; j++)
+            {
+                //color = color.grayscale;
+                a = colorValues[i, j];
+                //Debug.Log(a);
+                Color color = new Color(a, a, a);
+                texture.SetPixel(i, j, color);
+            }
+        }
+        
+
+        texture.filterMode = FilterMode.Point;
+        texture.Apply();
+        Renderer rnd = plane.GetComponent<Renderer>();
+        rnd.sharedMaterial.mainTexture = texture;
+    }
+
+    public int[] ConvertX(Vector3 [] data)
+    {
+        int[] intArray = new int[data.Length];
+        for(int i = 0; i <data.Length; i++)
+        {
+            intArray[i] = (int)(100 - data[i].x);
+           
+        }
+
+        return intArray;
+    }
+
+    public int[] HowManyInts(int[] data)
+    {
+        int[] ints = new int[200];
+        for (int i = 0; i < 200; i++)
+        {
+            foreach (int x in data)
+            {
+                if (x == i)
+                {
+                    ints[i] += 1;
+                }
+            }
+        }
+
+        return ints;
+    }
+
+
+
+    /*
     public void CreateTexture()
     {
         var texture = new Texture2D(20, 20);
@@ -199,7 +276,7 @@ public class HeatMap : MonoBehaviour
                 color = Color.black;
 
                 texture.SetPixel(i, j, color);
-               
+
             }
         }
         texture.filterMode = FilterMode.Point;
@@ -207,7 +284,35 @@ public class HeatMap : MonoBehaviour
         Renderer rnd = plane.GetComponent<Renderer>();
         rnd.sharedMaterial.mainTexture = texture;
     }
+    */
 
+
+    
+    private float[,] InitializeColorValues(int[] valueX)
+    {
+        float[,] colorValues = new float[200, 200];
+        //float valueX = 0;
+        float valueY = 0;
+        for (int i = 0; i < 200; i++)
+        {
+            valueY = 0f;
+            for(int j = 0; j < 200; j++)
+            {
+                if (valueX[i] > 0)
+                {
+                    colorValues[i, j] = 1f;
+                }
+                else
+                {
+                    colorValues[i, j] = 0f;
+                }
+                
+                //valueY+= 0.005f;
+            }
+            
+        }
+        return colorValues;
+    }
 
 
 
