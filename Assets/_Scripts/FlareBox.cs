66b1b761ec817b45f85ea2f8eb030c9042e60c17
PumbaaSWE <ballejalle@hotmail.com>
2024-12-12 00:19:14 +0100

    Added death events
    Syringes and Flareboxes now indicate if you try and pick up while maxed out
    New triggerbox mat
    Fixed level3 so music nodes are not bullet proof
    Added OnEnemyDeath object that fires event when enemies die. Can be disabled to not fire events prematurly or after certain point in level progression.

--- Assets/_Scripts/FlareBox.cs
+++ Assets/_Scripts/FlareBox.cs
@@ -1,7 +1,24 @@
 using UnityEngine;
 
-public class FlareBox : MonoBehaviour
+public class FlareBox : MonoBehaviour, IInteractable
 {
+
+    public string TooltipOnCan = "Press {0} to pick up flares";
+    public string TooltipOnFull = "You max amount of flares";
+
+
+    public string Tooltip { get; set; }
+
+    public string InteractedTooltip { get; set; }
+
+    public bool CanInteract => true;
+
+    public bool ShowInteractMessage => true;
+
+    public int InteractedDisplayPriority => 1;
+
+    public float InteractedTipDisplayTime => 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,8 +29,34 @@ public class FlareBox : MonoBehaviour
     public void PickUp(Transform obj)
     {
         if (obj.TryGetComponent(out FlareThrower flareThrower)) {
+            if (flareThrower.NumFlares >= flareThrower.MaxNumFlares)
+            {
+                InteractedTooltip = "Lol nope...";
+                return;
+            }
             flareThrower.NumFlares += 5;
             Destroy(gameObject);
+            InteractedTooltip = string.Empty;
+        }
+    }
+
+    public void Interact(Transform interactor)
+    {
+        PickUp(interactor);
+    }
+
+    public void SpeculateInteract(Transform interactor)
+    {
+        if (interactor.TryGetComponent(out FlareThrower flareThrower))
+        {
+            if(flareThrower.NumFlares >= flareThrower.MaxNumFlares)
+            {
+                Tooltip = TooltipOnFull;
+            }
+            else
+            {
+                Tooltip = TooltipOnCan;
+            }
         }
     }
 }
--- Assets/_Scripts/Syrringe.cs
+++ Assets/_Scripts/Syrringe.cs
@@ -1,9 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Syrringe : MonoBehaviour
+public class Syrringe : MonoBehaviour, IInteractable
 {
+
+
+    public string TooltipOnCan = "Press {0} to pick up syringe";
+    public string TooltipOnFull = "You max amount of syringes";
+
+
+    public string Tooltip { get; set; }
+
+    public string InteractedTooltip { get; set; }
+
+    public bool CanInteract => true;
+
+    public bool ShowInteractMessage => true;
+
+    public int InteractedDisplayPriority => 1;
+
+    public float InteractedTipDisplayTime => 1;
+
+    public void Interact(Transform interactor)
+    {
+        PickUp(interactor);
+    }
+
     public void PickUp(Transform obj)
     {
         if (obj.TryGetComponent(out SyrringeUser syrringeUser))
@@ -13,6 +34,23 @@ public class Syrringe : MonoBehaviour
 
             syrringeUser.NumSyrringes++;
             Destroy(gameObject);
+        }
+    }
+
+    public void SpeculateInteract(Transform interactor)
+    {
+        if (interactor.TryGetComponent(out SyrringeUser syrringeUser))
+        {
+            if (syrringeUser.NumSyrringes >= syrringeUser.MaxSyrringes)
+            {
+                Tooltip = TooltipOnFull;
+            }
+            else
+            {
+                Tooltip = TooltipOnCan;
+            }
+
+            
         }
     }
 }
